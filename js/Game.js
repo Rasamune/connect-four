4ad90742a15a09624a8dc95da703184a314c523a
Christopher Jaeger <cjaeger.ld@gmail.com>
2020-12-08 13:44:09 -0500

    Added rendering for tokens + board

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,38 @@
 class Game {
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
 
-    
+    /**
+     * Returns active player
+     * @returns   {Object}    The active player
+     */
+    get activePlayer() {
+        const activePlayer = this.players.find(player => player.active);
+        return activePlayer;
+    }
+
+    /** 
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+    createPlayers() {
+        const players = [new Player('Player 1', '#e15258', 1, true),
+                         new Player('Player 2', '#e59a13', 2)];
+        return players;
+    }
+
+    /*
+     * Begins Game
+     */
+    startGame() {
+        /** 
+         * Initializes game. 
+         */
+        this.board.drawHTMLBoard();
+        this.activePlayer.activeToken.drawHTMLToken();
+        this.ready = true;
+    }
 }
--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,19 @@ class Token {
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
     }
+
+    /** 
+     * Render Token
+     */
+    drawHTMLToken() {
+        const token = document.createElement('div');
+        document.querySelector('#game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class', 'token');
+        token.style.backgroundColor = this.owner.color;
+    }
+
+    get htmlToken() {
+        return this.drawHTMLToken();
+    }
 }
